8a719da7bfa87acd6e90a0b254d6cb3d46d10e51
Qanqae <ayeleqanqe2@gmail.com>
2025-07-02 01:10:21 +0300

    Update: UI + logic for 777 home spin, check-in modal, paid spin0

--- FreeSlots/vite.config.js
+++ FreeSlots/vite.config.js
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+   build: {
+    rollupOptions: {
+      external: ['@twa-dev/sdk'], // ⛔ Don't bundle this
+    },
+  },
   server: {
     headers: {
       'Access-Control-Allow-Origin': '*',
